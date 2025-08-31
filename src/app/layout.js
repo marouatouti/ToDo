18c6ee775700ac1a0c5a3b9ad07fb79cc7791cf8
marouatouti <marouatouoti2@gmail.com>
2025-08-31 16:11:25 +0200

    add bootstrap

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,9 +18,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+         <header>Je suis le header</header>
+        {children} {/* Ici s’affiche le contenu de chaque page */}
+        <footer>Je suis le footer</footer>
       </body>
     </html>
   );
